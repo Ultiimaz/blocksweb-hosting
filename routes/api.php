0d9023f932346db7a89a471780f052c0316e8331
Ultiimaz <Sven-tjeerd-sma@hotmail.com>
2022-11-18 21:59:57 +0100

    assets refactor 2

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('workspace/{workspace}',[AssetController::class, 'store']);
-Route::get('workspace/{workspace}', [AssetController::class, 'index']);
+Route::post('workspace/{workspace}/assets',[AssetController::class, 'store']);
+Route::get('workspace/{workspace}/assets', [AssetController::class, 'index']);
